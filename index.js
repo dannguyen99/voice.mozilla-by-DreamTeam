0192a2f74553c96e6e5a6d4c9d0db092addbe131
Phong Vu <phongvu99@outlook.com>
2020-02-09 22:57:49 +0700

    NodeJS Learning Progress

--- index.js
+++ index.js
@@ -1,36 +1,71 @@
 require('dotenv').config();
-// import express to our app
-const express = require('express');
+
+const User = require('./models/User');
+
 const path = require('path');
+const bodyParser = require('body-parser');
+const express = require('express');
 const app = express();
+
+const rootDir = require('./util/path');
+const mongoose = require('mongoose');
+
+const admin = require('./routes/admin');
+const shop = require('./routes/shop');
+
+const errorsController = require('./controllers/errors');
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(express.static(path.join(rootDir, 'public')));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
 app.use(express.urlencoded({
     extended: true
 }));
 
-const MongoClient = require('mongodb').MongoClient;
-// In my machine(ubuntu), mongodb runs locally at 127.0.0.1:27017
-// so you may need change the url based on your machine!
-MongoClient.connect('mongodb://127.0.0.1:27017',(err, client)=>{
-if (err) return console.log(err);
-// connect to mongodb instance, here I use 'test' database,
-// you can use any name!
-db = client.db('quotes');
+app.use((req, res, next) => {
+    User.findById('5e3fd0f2d4d7310fec14eb7c')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
 });
 
-const routes = require('./routes/');
-app.use('/', routes);
+app.use('/admin', admin);
+app.use(shop);
 
-app.use(express.static('public'));
-// app.use('/static', express.static('public'))
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-    res.send('Welcome to VoiceViet!');
-})
+app.use(errorsController.show_404);
 
 const port = process.env.PORT;
-app.listen(port, () => {
-    console.log(module.exports);
-    console.log('App started on port: ', port);
-})
+const DB_PASSWORD = process.env.DB_PASSWORD;
+// Connection URI
+const uri = `mongodb+srv://phongvu99:${DB_PASSWORD}@nodejs-course-6joar.mongodb.net/shop?retryWrites=true&w=majority`;
+
+mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(async (result) => {
+        // console.log('Mongoose connect', result);
+        const user = await User.findOne();
+        if (!user) {
+            let user = new User({
+                name: 'Phong',
+                email: 'phongvu99@outlook.com',
+                cart: {
+                    items: []
+                }
+            });
+            await user.save();
+        }
+        app.listen(port, () => {
+            console.log('App started on port:', port);
+        })
+    }).catch((err) => {
+        console.log(err);
+    });
--- routes/fbi.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-router.get('/fbi', (req, res) => {
-    res.render('fun');
-})
-
-module.exports = router;
