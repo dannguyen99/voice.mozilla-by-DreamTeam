7f23e693d5c266d444990294e366041edb6a76e9
Phong Vu <phongvu99@outlook.com>
2020-03-03 00:00:43 +0700

    VoiceViet recording testing

--- index.js
+++ index.js
@@ -1,71 +1,28 @@
 require('dotenv').config();
 
-const User = require('./models/User');
-
-const path = require('path');
-const bodyParser = require('body-parser');
 const express = require('express');
+const path = require('path');
+const rootDir = require('./util/path');
+const bodyParser = require('body-parser');
+const recorder = require('./routes/recorder');
+
 const app = express();
 
-const rootDir = require('./util/path');
-const mongoose = require('mongoose');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 
-const admin = require('./routes/admin');
-const shop = require('./routes/shop');
-
-const errorsController = require('./controllers/errors');
-
-app.set('view engine', 'ejs');
-app.set('views', 'views');
 app.use(express.static(path.join(rootDir, 'public')));
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}))
+app.set('view engine', 'ejs'); // EJS is supported by default
+app.set('views', 'views'); // Explicitly set
 
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(recorder);
 
-app.use((req, res, next) => {
-    User.findById('5e3fd0f2d4d7310fec14eb7c')
-        .then(user => {
-            req.user = user;
-            next();
-        })
-        .catch(err => console.log(err));
+const port = process.env.PORT; 
+
+app.listen(port || process.env.PORT, () => {
+    console.log('App started on port:', port);
 });
 
-app.use('/admin', admin);
-app.use(shop);
 
-app.use(errorsController.show_404);
 
-const port = process.env.PORT;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-// Connection URI
-const uri = `mongodb+srv://phongvu99:${DB_PASSWORD}@nodejs-course-6joar.mongodb.net/shop?retryWrites=true&w=majority`;
-
-mongoose.connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(async (result) => {
-        // console.log('Mongoose connect', result);
-        const user = await User.findOne();
-        if (!user) {
-            let user = new User({
-                name: 'Phong',
-                email: 'phongvu99@outlook.com',
-                cart: {
-                    items: []
-                }
-            });
-            await user.save();
-        }
-        app.listen(port, () => {
-            console.log('App started on port:', port);
-        })
-    }).catch((err) => {
-        console.log(err);
-    });
