83b73686205b753b0b82f2d54e810fb03d8ca2e0
ducddm <ducddm.b8042@st.usth.edu.vn>
2020-03-18 19:15:08 +0700

    Some changes in main-feature component

--- src/app/main-features/main-features.component.ts
+++ src/app/main-features/main-features.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-features',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainFeaturesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit() {
   }
